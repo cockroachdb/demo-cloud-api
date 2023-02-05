a3e259bb3e3f392d02d80d9a971ddfb2f6f5fd76
Paul Scanlon <paul@crlMBP-NW016XCPYWMjk3.local>
2023-02-05 21:52:05 +0000

    test: add env var to user session

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -2,18 +2,19 @@ import NextAuth from 'next-auth';
 import GithubProvider from 'next-auth/providers/github';
 
 export const authOptions = {
-  callbacks: {
-    session({ session }) {
-      session.user.admin = session.user.id === process.env.GITHUB_ADMIN_USER ? true : false;
-      return session;
-    },
-  },
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
     }),
   ],
+  callbacks: {
+    session({ session }) {
+      session.user.admin = session.user.id === process.env.GITHUB_ADMIN_USER ? true : false;
+      session.user.test = process.env.GITHUB_ADMIN_USER;
+      return session;
+    },
+  },
 };
 
 export default NextAuth(authOptions);
