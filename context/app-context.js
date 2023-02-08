0d71e1f1c038fecaa42a3b5a843374adbd039992
Paul Scanlon <paul@crlMBP-NW016XCPYWMjk3.local>
2023-02-08 15:32:01 +0000

    feat: add NEXT_PUBLIC_API_URL to all-clusters api path

--- context/app-context.js
+++ context/app-context.js
@@ -2,6 +2,8 @@ import React, { createContext, useState } from 'react'
 import { useSession } from 'next-auth/react'
 import { useQuery } from '@tanstack/react-query'
 
+const isProd = process.env.NODE_ENV === 'production'
+
 export const AppContext = createContext()
 
 export const AppProvider = ({ children }) => {
@@ -11,7 +13,7 @@ export const AppProvider = ({ children }) => {
   const { status, data: clusters } = useQuery({
     queryKey: ['clusters'],
     queryFn: async () => {
-      const response = await fetch('/api/cloud/all-clusters')
+      const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/cloud/all-clusters`)
 
       if (!response.ok) {
         throw new Error('Bad Response')
