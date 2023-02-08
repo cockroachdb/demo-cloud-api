eda0a9e8f7a4870b5a43e3ffc452cbdf460358da
Paul Scanlon <paul@crlMBP-NW016XCPYWMjk3.local>
2023-02-08 15:32:35 +0000

    chore: remove rewrites from next.config

--- next.config.js
+++ next.config.js
@@ -1,13 +1,5 @@
 const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = {
-  assetPrefix: isProd ? process.env.NEXT_PUBLIC_ASSET_PREFIX : undefined,
-  async rewrites() {
-    return [
-      {
-        source: '/api/cloud',
-        destination: isProd ? `${process.env.NEXTAUTH_URL}/api/cloud` : undefined
-      }
-    ]
-  }
+  assetPrefix: isProd ? process.env.NEXT_PUBLIC_ASSET_PREFIX : undefined
 }
