82ae811e993bb3f09213e9b36837dc051e2d14f8
Paul Scanlon <paul@crlMBP-NW016XCPYWMjk3.local>
2023-02-08 15:14:55 +0000

    feat: add rewrites for api paths

--- next.config.js
+++ next.config.js
@@ -1,5 +1,13 @@
 const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = {
-  assetPrefix: isProd ? process.env.NEXT_PUBLIC_ASSET_PREFIX : undefined
+  assetPrefix: isProd ? process.env.NEXT_PUBLIC_ASSET_PREFIX : undefined,
+  async rewrites() {
+    return [
+      {
+        source: '/api/cloud',
+        destination: isProd ? `${process.env.NEXTAUTH_URL}/api/cloud` : undefined
+      }
+    ]
+  }
 }
