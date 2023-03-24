c1189a6fe2222a114d2ea236302ec3204f3a2a51
Paul Scanlon <pauliescanlon@gmail.com>
2023-03-24 20:10:02 +0000

    chore: add google analytics test 1

--- pages/_document.js
+++ pages/_document.js
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import Script from 'next/script'
 import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
@@ -9,6 +9,19 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
+        <Script
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GTM}`}
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', '${process.env.NEXT_PUBLIC_GTM}');
+        `}
+        </Script>
       </body>
     </Html>
   )
